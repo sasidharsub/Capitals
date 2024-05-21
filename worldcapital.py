9c056d82f995c90cab9c5fe6219fb383040ec6e4
sasidharsub <95578948+sasidharsub@users.noreply.github.com>
2024-05-21 08:12:38 +0530

    Update worldcapital.py
    Code optimised

--- worldcapital.py
+++ worldcapital.py
@@ -1,29 +1,46 @@
+import pandas as pd
+
 def capitalcities(user_input):   
-    import pandas as pd
+    # Read the table from the Wikipedia page
     caps = pd.read_html('https://en.wikipedia.org/wiki/List_of_national_capitals')
     capital = caps[1]
-    capital.drop('Notes', axis = 1, inplace = True)
+    
+    # Drop the 'Notes' column and unnecessary rows
+    capital = capital.drop(columns=['Notes']). drop([0, 62])
+    
+    # Rename the columns for better readability
     capital.columns = ['Capital', 'Country']
-    capital[capital['Country'] == 'Sri Lanka']
-    capital.drop([0,62], inplace=True)
-    def remove_extra(x):
-        for i in capital.Country:
-            if x in i:
-                capital.Country.replace({ i : x}, inplace = True)
-    remove_extra('Israel')
-    remove_extra('Palestine')
-    remove_extra('Western Sahara')
-    remove_extra('Kosovo')
-    capital.Country.replace({ 'Cocos (Keeling) Islands' : 'Cocos Islands', 'Guinea-Bissau':'Guinea Bissau'}, inplace = True)
-    for i in capital.Country:
-        capital.Country.replace({i : i.lower()}, inplace = True)
-    Country = capital.Country.tolist()
-    Capital = capital.Capital.tolist()
-    while True:
-        user_input = user_input.lower()
-        if user_input in Country:
-            index = Country.index(user_input)
-            return f'{Capital[index]}'
-            break
-        else:
-            return 'Call the function with the country name to get the capital'
+    
+    # Function to replace country names
+    def replace_country_names(old_name, new_name):
+        capital['Country'] = capital['Country'].str.replace(old_name, new_name)
+    
+    # Replace specific country names
+    replacements = {
+        'Israel': 'Israel',
+        'Palestine': 'Palestine',
+        'Western Sahara': 'Western Sahara',
+        'Kosovo': 'Kosovo',
+        'Cocos (Keeling) Islands': 'Cocos Islands',
+        'Guinea-Bissau': 'Guinea Bissau'
+    }
+    
+    for old_name, new_name in replacements.items():
+        replace_country_names(old_name, new_name)
+    
+    # Convert country names to lowercase
+    capital['Country'] = capital['Country'].str.lower()
+    
+    # Create lists of countries and their capitals
+    countries = capital['Country'].tolist()
+    capitals = capital['Capital'].tolist()
+    
+    # Convert user input to lowercase
+    user_input = user_input.lower()
+    
+    # Find and return the capital city if the country is in the list
+    if user_input in countries:
+        index = countries.index(user_input)
+        return capitals[index]
+    else:
+        return 'Call the function with the country name to get the capital'
